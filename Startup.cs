7fe6bb45804f5ce8b2568a7b0f1ad8099d486727
Mauricio Apaza <mpacarpio@gmail.com>
2022-01-03 23:19:29 -0500

    indexado

--- Startup.cs
+++ Startup.cs
@@ -64,7 +64,7 @@ namespace ManitasWeb
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Home}/{action=Index}");
                 endpoints.MapRazorPages();
             });
         }
